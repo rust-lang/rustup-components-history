308804abf5672466f06f59bca2a9cf51f611bbef
mexus <gilaldpellaeon@gmail.com>
2019-03-08 21:24:03 +0300

    Fix travis build

--- prepare-pages.sh
+++ prepare-pages.sh
@@ -1,7 +1,15 @@
 #!/usr/bin/env sh
 
+# Fail on errors
 set -e -u
 
+# Build the project
+cargo build --verbose --all
+
+# Test everything
+cargo test --verbose --all
+
+# Generate the pages
 mkdir output
 cargo run --release --bin=rustup-available-packages-html -- render -c config.yaml
 ln -s x86_64-unknown-linux-gnu.html output/index.html
