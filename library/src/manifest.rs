474de60236d7f297143a31b4b9748182ffd18429
onur-ozkan <work@onurozkan.dev>
2024-08-12 17:14:44 +0300

    remove `rls`
    Signed-off-by: onur-ozkan <work@onurozkan.dev>

--- library/src/manifest.rs
+++ library/src/manifest.rs
@@ -97,9 +97,6 @@ available = false
 [pkg.rustfmt-preview.target.x86_64-unknown-linux-gnu]
 available = true
 
-[renames.rls]
-to = "rls-preview"
-
 [renames.rustfmt]
 to = "rustfmt-preview"
 "#;
@@ -152,20 +149,12 @@ to = "rustfmt-preview"
             ]
             .into_iter()
             .collect(),
-            renames: vec![
-                (
-                    "rls".to_string(),
-                    Rename {
-                        to: "rls-preview".to_string(),
-                    },
-                ),
-                (
-                    "rustfmt".to_string(),
-                    Rename {
-                        to: "rustfmt-preview".to_string(),
-                    },
-                ),
-            ]
+            renames: vec![(
+                "rustfmt".to_string(),
+                Rename {
+                    to: "rustfmt-preview".to_string(),
+                },
+            )]
             .into_iter()
             .collect(),
         };
